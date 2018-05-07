c7cf5425003ca36496742c6a8c3d794efb6e05e6
JoseFlavio <jflavio90@gmail.com>
2018-05-07 09:44:36 -0500

    added mqtt job service implementation

--- app/src/main/java/com/jflavio1/androidmqttexample/mqtt/SensorsMqttService.kt
+++ app/src/main/java/com/jflavio1/androidmqttexample/mqtt/SensorsMqttService.kt
@@ -1,15 +1,13 @@
 package com.jflavio1.androidmqttexample.mqtt
 
-import android.app.job.JobParameters
-import android.app.job.JobService
 import android.os.Build
-import android.provider.Settings
 import android.support.annotation.RequiresApi
 import android.util.Log
+import com.firebase.jobdispatcher.JobParameters
+import com.firebase.jobdispatcher.JobService
 import org.eclipse.paho.client.mqttv3.IMqttActionListener
-import org.eclipse.paho.client.mqttv3.IMqttMessageListener
 import org.eclipse.paho.client.mqttv3.IMqttToken
-import org.eclipse.paho.client.mqttv3.MqttMessage
+
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 /**
@@ -28,18 +26,17 @@ class SensorsMqttService : JobService(), BaseMqttModel {
         val MQTT_CONNECT = "mqtt_connect"
         val MQTT_DISCONNECT = "mqtt_disconnect"
 
-        // TODO server url
-        val MQTT_SERVER_URL = ""
+        val MQTT_SERVER_URL = "broker.hivemq.com"
     }
 
     // TODO have a VIEW representation here?
 
     override fun onStartJob(params: JobParameters?): Boolean {
-        this.mqttCliendId = System.getenv(Settings.Secure.ANDROID_ID)
+        this.mqttCliendId = Build.SERIAL
 
-        if (MQTT_CONNECT == params!!.extras.getString(MQTT_START_REASON)) {
+        if (MQTT_CONNECT == params!!.extras!!.getString(MQTT_START_REASON)) {
             connectToServer()
-        } else if (MQTT_DISCONNECT == params.extras.getString(MQTT_START_REASON)) {
+        } else if (MQTT_DISCONNECT == params.extras!!.getString(MQTT_START_REASON)) {
             disconnectFromServer()
         }
 
@@ -73,9 +70,11 @@ class SensorsMqttService : JobService(), BaseMqttModel {
 
         this.mqttClient.subscribe(topicName, 0, this, object : IMqttActionListener {
             override fun onSuccess(asyncActionToken: IMqttToken?) {
+                logMqtt("Success subscribing to topic $topicName")
             }
 
             override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                logMqtt("Failure on topic subscription")
             }
         }) { topic, message -> }
 
--- app/src/main/java/com/jflavio1/androidmqttexample/presenters/SensorsListPresenterImpl.kt
+++ app/src/main/java/com/jflavio1/androidmqttexample/presenters/SensorsListPresenterImpl.kt
@@ -1,6 +1,15 @@
 package com.jflavio1.androidmqttexample.presenters
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context.JOB_SCHEDULER_SERVICE
+import android.os.Build
+import android.util.Log
+import com.firebase.jobdispatcher.*
+import com.jflavio1.androidmqttexample.mqtt.SensorsMqttService
 import com.jflavio1.androidmqttexample.views.SensorsListView
+
 
 /**
  * SensorsListPresenterImpl
@@ -10,12 +19,51 @@ import com.jflavio1.androidmqttexample.views.SensorsListView
  */
 class SensorsListPresenterImpl(val view: SensorsListView) : SensorsListPresenter {
 
+    private val TAG = "SensorsPresenter"
+
     init {
         this.view.setSensorPresenter(this)
     }
 
     override fun initMqttService() {
-        // TODO init mqtt service
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            val dispatcher = FirebaseJobDispatcher(GooglePlayDriver(this.view.getViewContext()))
+
+            val job = dispatcher.newJobBuilder()
+                    // persist the task across boots
+                    .setLifetime(Lifetime.FOREVER)
+                    // Call this service when the criteria are met.
+                    .setService(SensorsMqttService::class.java)
+                    // unique id of the task
+                    .setTag("OneTimeJob")
+                    // We are mentioning that the job is not periodic.
+                    .setRecurring(false)
+                    // Run between 30 - 60 seconds from now.
+                    .setTrigger(Trigger.executionWindow(30, 60))
+                    //Run this job only when the network is avaiable.
+                    .setConstraints(Constraint.ON_ANY_NETWORK)
+                    .build()
+
+            dispatcher.mustSchedule(job)
+
+        } else {
+
+            val componentName = ComponentName(this.view.getViewContext(), SensorsMqttService::class.java)
+            val jobInfo = JobInfo.Builder(12, componentName)
+                    .setRequiresCharging(true)
+                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                    .build()
+
+            val jobScheduler = this.view.getViewContext().getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+            val resultCode = jobScheduler.schedule(jobInfo)
+            if (resultCode == JobScheduler.RESULT_SUCCESS) {
+                Log.d(TAG, "Job scheduled!")
+            } else {
+                Log.d(TAG, "Job not scheduled")
+            }
+
+        }
     }
 
     override fun getTemperatures() {
