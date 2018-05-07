a8d9ef328faa9f274256b11df2e92732716cd919
JoseFlavio <jflavio90@gmail.com>
2018-05-06 22:47:58 -0500

    added sensor list presenter

---
+++ app/src/main/java/com/jflavio1/androidmqttexample/presenters/SensorsListPresenter.kt
@@ -0,0 +1,12 @@
+package com.jflavio1.androidmqttexample.presenters
+
+/**
+ * SensorsListPresenter
+ *
+ * @author Jose Flavio - jflavio90@gmail.com
+ * @since  6/5/17
+ */
+interface SensorsListPresenter {
+    
+
+}
